6c73d4b368dd51bf49adb39ba1d80a7be8096670
shanza99 <shanzabatool001@gmail.com>
2024-11-05 09:20:31 +0500

    add

--- task/lib/today_tasks.dart
+++ task/lib/today_tasks.dart
@@ -8,11 +8,9 @@ class TodayTasks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Get today's date without the time component
     final today = DateTime.now();
     final todayOnlyDate = DateTime(today.year, today.month, today.day);
 
-    // Filter tasks where dueDate matches today’s date (ignoring time)
     final todayTasks = tasks.where((task) {
       final taskDateOnly = DateTime(task.dueDate.year, task.dueDate.month, task.dueDate.day);
       return taskDateOnly == todayOnlyDate && !task.isCompleted && !task.isRepeated;
