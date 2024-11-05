9991ebabcd27c53ee409e308a1e504e32b794270
shanza99 <shanzabatool001@gmail.com>
2024-11-05 09:06:35 +0500

    update task

--- task/lib/task.dart
+++ task/lib/task.dart
@@ -4,6 +4,8 @@ class Task {
   DateTime dueDate;
   bool isCompleted;
   bool isRepeated;
+  List<String> subtasks;
+  int completedSubtasks;
 
   Task({
     required this.title,
@@ -11,5 +13,9 @@ class Task {
     required this.dueDate,
     this.isCompleted = false,
     this.isRepeated = false,
+    this.subtasks = const [],
+    this.completedSubtasks = 0,
   });
+
+  double get progress => (completedSubtasks / subtasks.length).clamp(0, 1);
 }
